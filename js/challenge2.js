adbc020300fc6c90d427d0e715a81f31d76a3af3
Olajide Jegede <33847734+ojjegede@users.noreply.github.com>
2019-11-26 21:52:58 -0500

    Update challenge2.js

--- js/challenge2.js
+++ js/challenge2.js
@@ -1,12 +1,10 @@
-function setHomeAddress(){
-	if ($("#useBilling").is(":checked")){
-		$("#home").val($("#billing").val());
-		$("#home").prop('disabled',true);
-  }
-	else {
-
-		$("#home").val("");
-		$("#home").prop('disabled',false);
-  }
-
+$(useBilling).click(function(){
+if(this.checked){
+	$("#home").val($("#billing").val());
+	$("#home").prop("disabled",true);
 }
+else{
+	$("#home").val(""); 
+	$("#home").prop("disabled",false);
+}
+});
