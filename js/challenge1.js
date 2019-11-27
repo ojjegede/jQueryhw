2c1778d867ccc0ea379031e63ac666e08f357696
Olajide Jegede <33847734+ojjegede@users.noreply.github.com>
2019-11-26 21:52:20 -0500

    Update challenge1.js

--- js/challenge1.js
+++ js/challenge1.js
@@ -1,9 +1,10 @@
-$(document).change(function(){
-    if ($(this).is(':checked')){
-        console.log("selected");
-        $("#emailField").css("display", "block");
-    } else {
-        console.log("Not selected");
-        $("emailField").css("display", "none")
-    }
-})
+$(subscribe).click(function(){
+	if (this.checked){
+		console.log("selected"); 
+		$("#emailField").css("display","block"); 
+	}
+	else{
+		console.log("Not selected"); 
+		$("#emailField").css("display","none"); 
+	}
+});
