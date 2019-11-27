f1fdb1d0f0f2aa4867293d8e2bdfd7d93f5c95bc
Olajide Jegede <33847734+ojjegede@users.noreply.github.com>
2019-11-26 21:56:17 -0500

    Update challenge5.js

--- js/challenge5.js
+++ js/challenge5.js
@@ -1,25 +1,22 @@
   
-console.log("here")
-var imgs = $("img");
-var msg = "Hover over an image below."
-	imgs.each(function(){
-		$(this).on("mousemove",function() {
-		 	$('#image').css("backgroundImage", "url('"+$(this).attr('src')+"')");
-		 	$('#image').html($(this).attr('alt'));
-	 	})
+console.log("here");
+var msg = "Hover over an image below.";
+$('img').each(function(){
+	$(this).mouseenter(function(){
+		$("#image").css("backgroundImage","url('"+$(this).attr('src')+"')");
+		$("#image").html($(this).attr('alt'));
+	});
+	$(this).focus(function(){
+		$("#image").css("backgroundImage","url('"+$(this).attr('src')+"')");
+		$("#image").html($(this).attr('alt'));
+	});
+	$(this).mouseleave(function(){
+		$("#image").css("backgroundImage","url('')");
+		$("#image").html(msg);
+	});
+	$(this).blur(function(){
+		$("#image").css("backgroundImage","url('')");
+		$("#image").html(msg);
+	});
 
-	 	$(this).focus(function() {
-			$('#image').css("backgroundImage", "url('"+$(this).attr('src')+"')");
-		 	$('#image').html($(this).attr('alt'));
-	 	})
-
-	 $(this).mouseleave(function() {
-		 	$('#image').css("backgroundImage", "url('')");
-		 	$('#image').html(msg);
-	 })
-	
-	 $(this).blur(function() {
-		 $('#image').css("backgroundImage", "url('')");
-		 $('#image').html(msg);
-	})
-})
+});
